51829b975fc52d027bd2dff5255cb641a7ac4166
Md Abdullah Al Mamun <39150506+Mamun5011@users.noreply.github.com>
2026-06-11 14:30:09 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # lower bound is 1 banana and upper bound is max(nums) banana
+        l = 1
+        r = max(piles)
+        # set initial result as highest possible banana
+        res = r
+        # binary search for banana count which gives us the lowest banana count
+        while l <= r:
+            #midpoint
+            mid = (l+r)//2
+            #getting the time to finish all piles
+            time = sum([math.ceil(x/mid) for x in piles])
+            #if the time than threshold, update result and eliminate right portion of array
+            if  time <= h:
+                res = min(res, mid)
+                r = mid - 1
+            else:  # time needs to reduce, so increase banana count and so go beyond midpoint
+                l = mid + 1
+          
+        
+        return res
