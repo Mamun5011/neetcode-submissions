8f3032b9844f9826304790a6f3b32f3a362fdd28
Md Abdullah Al Mamun <39150506+Mamun5011@users.noreply.github.com>
2026-06-25 16:03:09 -0700

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,21 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.Capacity = capacity
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
+        if len(self.cache) > self.Capacity:
+            self.cache.popitem(last=False)
+        
