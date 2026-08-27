04a809180eca74aa737d7cf8793a6d906b810789
Md Abdullah Al Mamun <39150506+Mamun5011@users.noreply.github.com>
2026-08-26 23:39:43 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        res = []
+        preMap = collections.defaultdict(list)
+
+        for u, v in prerequisites:
+            preMap[u].append(v)
+
+        visited = set()   # courses in current DFS path
+        done = set()      # courses already fully processed
+
+        def dfs(course):
+            if course in visited:
+                return False
+
+            if course in done:
+                return True
+
+            visited.add(course)
+
+            for pre in preMap[course]:
+                if not dfs(pre):
+                    return False
+
+            visited.remove(course)
+            done.add(course)
+            res.append(course)
+
+            return True
+
+        for n in range(numCourses):
+            if not dfs(n):
+                return []
+
+        return res
+        
